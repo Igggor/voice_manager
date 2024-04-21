30298c52a74a7acb68c55acd701b403d9b91f882
SrormDevXK <mhstudio1912@gmail.com>
2024-04-22 02:51:03 +0300

    add func for api: edit and delete row in db. edit gitignore

--- Api/routes/index.js
+++ Api/routes/index.js
@@ -3,7 +3,6 @@ let router = express.Router()
 const {where} = require("sequelize")
 const seq = require('../dbmodels')
 
-/* GET home page. */
 router.get('/', async (req, res, next) => {
   console.log(req.body.table)
   let data = {}
@@ -38,6 +37,46 @@ router.post('/', async (req, res, next) => {
     data = {error: 'table not found'}
   }
   res.json(data)
+})
+
+router.put('/', async (req, res, next) => {
+  console.log(req.body)
+  let data = {}
+  if (seq.hasOwnProperty(req.body.table)) {
+    try {
+      data = await seq[req.body.table].update(JSON.parse(req.body.changes),{
+        where: JSON.parse(req.body.params)
+      })
+    }
+    catch (e) {
+      data = {error: e}
+    }
+  }
+  else {
+    data = {error: 'table not found'}
+  }
+  res.json(data)
+})
+
+router.delete('/', async (req, res, next) => {
+  console.log(req.body.table)
+  let data = {}
+  if (seq.hasOwnProperty(req.body.table)) {
+    try {
+      data = await seq[req.body.table].destroy({
+        where: JSON.parse(req.body.params)
+      })
+    }
+    catch (e) {
+      data = {error: e}
+    }
+  }
+  else {
+    data = {error: 'table not found'}
+  }
+  res.json(data)
+
+  console.log(req.body)
 })
 
 module.exports = router
