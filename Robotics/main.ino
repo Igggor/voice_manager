1f85930470496c1a42f34f13631a958619b03c6c
Svetogor <svetogor00@gmail.com>
2024-05-26 00:37:03 +0300

    Refactor code

--- Robotics/main.ino
+++ Robotics/main.ino
@@ -1,21 +1,27 @@
+#define MY_PERIOD 10    // период в мс
+#define LED_PIN 14      // номер пина светодиода
+
+boolean LEDflag = false;
+uint32_t myTimer; // переменная времени
+String str;
+
 void setup() {
-  pinMode(14, OUTPUT);
-  digitalWrite(14,0);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
   Serial.begin(115200);
   Serial1.begin(9600);
 }
 
 void loop() {
-  
-  if(Serial1.available()){
-    String str;
-    delay(10);
-    while(Serial1.available()) str+=String(char(Serial1.read()));
+  if(Serial1.available() && millis() - myTimer >= MY_PERIOD){
+    while(Serial1.available()) {
+      str += String(char(Serial1.read()));
+    }
     Serial.println(str);
     if(str=="None"){}
-    else if(str=="On") digitalWrite(14,1);
-    else if(str=="Off") digitalWrite(14,0);
+    else if(str=="On") digitalWrite(LED_PIN, HIGH);
+    else if(str=="Off") digitalWrite(LED_PIN, LOW);
+    myTimer = millis();         // сбросить таймер
   }
-  
 }
